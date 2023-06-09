3971d92234f0ced283841e783a7554fbe7737891
ukoricpaw <phero8121324@gmail.com>
2023-06-09 19:48:12 +0600

    fixed a bug

--- db.js
+++ db.js
@@ -2,11 +2,14 @@ const { Sequelize } = require('sequelize');
 require("dotenv").config();
 
 
-const { DB_NAME, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT } = process.env
+const { DATABASE_URL } = process.env
 
-module.exports = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
+module.exports = new Sequelize(DATABASE_URL, {
   dialect: "postgres",
-  host: DB_HOST,
-  port: DB_PORT,
-  ssl: true
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 })
