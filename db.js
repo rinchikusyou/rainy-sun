b0f9bfb5d702e7a3177f1168f369c9a6fa2b6e5e
ukoricpaw <phero8121324@gmail.com>
2023-06-09 19:43:19 +0600

    fixed a bug

--- db.js
+++ db.js
@@ -7,5 +7,6 @@ const { DB_NAME, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT } = process.env
 module.exports = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
   dialect: "postgres",
   host: DB_HOST,
-  port: DB_PORT
+  port: DB_PORT,
+  ssl: true
 })
