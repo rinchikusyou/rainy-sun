d4d74a6b8988d10ef8349291b088b5510927ebfd
ukoricpaw <phero8121324@gmail.com>
2023-06-10 15:46:05 +0600

    bugfix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const router = require("./routes/index")
 const fileUpload = require("express-fileupload")
 const ErrorHandlerMiddleware = require("./middlewares/ErrorHandlerMiddleware")
-// const checkRefererMiddleware = require("./middlewares/checkReferer");
+const checkRefererMiddleware = require("./middlewares/checkReferer");
 const cors = require("cors")
 const path = require("path")
 const fs = require('fs');
@@ -22,7 +22,7 @@ app.use(cors());
 app.use(express.json());
 app.use(fileUpload({}));
 app.use(express.static(staticPath))
-// app.use(checkRefererMiddleware)
+app.use(checkRefererMiddleware)
 app.use("/api", router);
 app.use(ErrorHandlerMiddleware);
 
