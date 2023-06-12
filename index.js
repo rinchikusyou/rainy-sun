ca4d75d4a924eb8f132e3dd5ee65b78ae67f2394
ukoricpaw <phero8121324@gmail.com>
2023-06-12 22:56:15 +0600

    bugfix

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(fileUpload({}));
-// app.use(checkRefererMiddleware)
+app.use(checkRefererMiddleware)
 app.use("/api", router);
 app.use(ErrorHandlerMiddleware);
 
